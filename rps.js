471cecd5eb36576fba97b627a9a2d2ca69f12439
David josh0090 <davidjosh980@gmail.com>
2026-04-07 01:34:47 -0700

    first commit

--- rps.js
+++ rps.js
@@ -8,7 +8,6 @@ const outcome2 = document.getElementById('outcome2');
 
 let playerChoice = "";
 let compMove = "";
-let random = "";
 let you = 0;
 let comp = 0;
 let result = "";
@@ -18,31 +17,39 @@ function randomNum() {
 
     if (random <= 1/3) {
         compMove = "rock";
-    } else if (random <= 2/3) {
+        document.getElementById('compchoice').innerHTML =
+        `<button><i class="fa-solid fa-hand-back-fist"></i></button>`;
+    } 
+    else if (random <= 2/3) {
         compMove = "paper";
-    } else {
+        document.getElementById('compchoice').innerHTML =
+        `<button><i class="fa-solid fa-hand-paper"></i></button>`;
+    } 
+    else {
         compMove = "scissors";
+        document.getElementById('compchoice').innerHTML =
+        `<button><i class="fa-solid fa-hand-scissors"></i></button>`;
     }
 }
 
 btn1.addEventListener('click', () => {
     playerChoice = "rock";
     choice();
+    document.getElementById('yourchoice').innerHTML = `<button><i class="fa-solid fa-hand-back-fist"></i></button>`;
 });
 btn2.addEventListener('click', () => {
     playerChoice = "paper";
     choice();
+    document.getElementById('yourchoice').innerHTML =`<button><i class="fa-solid fa-hand-paper"></i></button>`;
 });
 btn3.addEventListener('click', () => {
     playerChoice = "scissors";
     choice();
+     document.getElementById('yourchoice').innerHTML = `<button><i class="fa-solid fa-hand-scissors"></i></button>`;
 });
 
 function choice() {
     randomNum();
-
-    outcome1.innerText = `You picked ${playerChoice}`;
-    outcome2.innerText = `Computer picked ${compMove}`;
 
     if (playerChoice === compMove) {
         return;
@@ -52,10 +59,12 @@ function choice() {
         if (compMove === "paper") {
             result = "loss";
             comp ++;
+            outcome1.innerText = `YOU LOSE`;
         } else
         if (compMove === "scissors") {
             result = "win";
             you ++;
+            outcome1.innerText = `YOU WON`;
         }
         yourscore.innerText = you;
         compscore.innerText = comp;
@@ -64,9 +73,11 @@ function choice() {
     if (playerChoice === "paper") {
         if (compMove === "rock") {
             you ++;
+            outcome1.innerText = `YOU WON`;
         } else
         if (compMove === "scissors") {
             comp ++;
+            outcome1.innerText = `YOU LOSE`;
         }
         yourscore.innerText = you;
         compscore.innerText = comp;
@@ -75,9 +86,13 @@ function choice() {
     if (playerChoice === "scissors") {
         if (compMove === "rock") {
             comp ++;
+            outcome1.innerText = `YOU LOSE`;
         } else
         if (compMove === "paper") {
             you ++;
+            outcome1.innerText = `YOU WON`;
         }
+        yourscore.innerText = you;
+        compscore.innerText = comp;
     }
 }
